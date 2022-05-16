3a51bd6dec04c5f8f5e68e798c7f36740f61114d
everr <everrwsr@outlook.com>
2022-05-16 19:02:40 +0800

    before add footer

--- .history/app_20220516151705.py
+++ .history/app_20220516151705.py
@@ -19,9 +19,3 @@ movies = [
 @app.route('/')
 def index():
     return render_template('index.html', name=name, movies=movies)
-
-
-@app.errorhandler(404)  # 传入要处理的错误代码
-def page_not_found(e):  # 接受异常对象作为参数
-    user = User.query.first()
-    return render_template('404.html', user=user), 404  # 返回模板和状态码
